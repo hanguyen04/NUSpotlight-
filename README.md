898c457d9458e12885292e998e9807074936218f
Jane Kamata <janekamata8@gmail.com>
2022-10-02 13:08:35 -0400

    Instructions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NUSpotlight-
+# npm install express
+# npm install nodemon
+# npx create-react-app demo
+# cd demo
+# npm start
--- demo/src/App.js
+++ demo/src/App.js
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Some text</p>
     </div>
   );
 }
